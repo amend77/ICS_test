b9bfac935a6fea98a188f30f874214c99deb4a40
Alexander Mendygaliev <109907088+amend77@users.noreply.github.com>
2023-04-25 13:00:51 +0400

    Добавлена таблица Basket

--- CreateStructure.sql
+++ CreateStructure.sql
@@ -17,3 +17,27 @@ CREATE TABLE dbo.Family (
 	BudgetValue DECIMAL(18, 2),
 	PRIMARY KEY(ID)
 );
+
+-- Создание таблицы Basket 
+	-- (ID identity, 
+	-- ID_SKU (внешний ключ на таблицу dbo.SKU), 
+	-- ID_Family (Внешний ключ на таблицу dbo.Family),
+	-- Quantity, 
+	-- Value, 
+	-- PurchaseDate, 
+	-- DiscountValue)
+-- Ограничение, что поле Quantity и Value не могут быть меньше 0
+-- Добавить значение по умолчанию для поля PurchaseDate: дата добавления записи (текущая дата)
+
+CREATE TABLE dbo.Basket (
+	ID INT IDENTITY,
+	ID_SKU INT,
+	ID_Family INT,
+	Quantity DECIMAL(18, 0) CHECK(Quantity >= 0),
+	Value DECIMAL(18, 2) CHECK(Value >= 0),
+	PurchaseDate DATE NOT NULL
+		CONSTRAINT DFT_orderdate DEFAULT(SYSDATETIME()),
+	DiscountValue DECIMAL(18, 2),
+	CONSTRAINT ID_SKU FOREIGN KEY(ID_SKU) REFERENCES dbo.SKU (ID),
+	CONSTRAINT ID_Family FOREIGN KEY(ID_Family) REFERENCES dbo.Family (ID)
+);
